2f039794b684cd83747010d865a609e7e1885bee
kurniawanew <kurniawanew@gmail.com>
2017-03-28 15:21:41 +0700

    penambahan library jquery dan bootstrap

--- lumen.js
+++ lumen.js
@@ -1,10 +1,10 @@
 var panjang, lebar, tinggi, substrate, putih, merah, biru, watt = 1, volAir;
 
 function ukuranTank() {
-	panjang = document.getElementById("panjang").value;
-   	lebar = document.getElementById("lebar").value;
-   	tinggi = document.getElementById("tinggi").value;
-   	substrate = document.getElementById('substrate').value;
+	panjang = $("#panjang").val();
+   	lebar = $("#lebar").val();
+   	tinggi = $("#tinggi").val();
+   	substrate = $('#substrate').val();
    	if (panjang != "" && lebar != "" && tinggi != "" && substrate != "") {
 		var volume = (panjang*lebar*tinggi)/1000;
 		var volSubstrate = (panjang * lebar * substrate)/1000;
@@ -12,15 +12,15 @@ function ukuranTank() {
 		var malang = (volSubstrate*0.6).toFixed(2);
 		var silika = (volSubstrate*1.2).toFixed(2);
 		volAir = (panjang * lebar * (tinggi - substrate))/1000;
-		document.getElementById("ukuran").innerHTML = panjang+" x "+lebar+" x "+tinggi;
-		document.getElementById("volume").innerHTML = volume+" liter";
-		document.getElementById("volair").innerHTML = volAir+" liter";
-		document.getElementById("filmin").innerHTML = volume*5+" l/h";
-		document.getElementById("filmax").innerHTML = volume*10+" l/h";
-		document.getElementById("volSubstrate").innerHTML =  volSubstrate +" liter";
-		document.getElementById("amazonia").innerHTML = "&plusmn; "+pecahan(amazonia) +" kantong";
-		document.getElementById("malang").innerHTML = "&plusmn; "+ pecahan(malang) +" kg";
-		document.getElementById("silika").innerHTML = "&plusmn; "+ pecahan(silika) +" kg";
+		$("#ukuran").text(panjang+" x "+lebar+" x "+tinggi);
+		$("#volume").text(volume+" liter");
+		$("#volair").text(volAir+" liter");
+		$("#filmin").text(volume*5+" l/h");
+		$("#filmax").text(volume*10+" l/h");
+		$("#volSubstrate").text(volSubstrate +" liter");
+		$("#amazonia").html("&plusmn; "+pecahan(amazonia) +" kantong");
+		$("#malang").html("&plusmn; "+ pecahan(malang) +" kg");
+		$("#silika").html("&plusmn; "+ pecahan(silika) +" kg");
 		dayaLed();
 		if (putih != undefined || merah != undefined || biru != undefined) {
 			hitungLumen();
@@ -29,9 +29,9 @@ function ukuranTank() {
 }
 
 function hitungLumen () {
-	putih = document.getElementById('putih').value;
-   	merah = document.getElementById('merah').value;
-   	biru = document.getElementById('biru').value;
+	putih = $('#putih').val();
+   	merah = $('#merah').val();
+   	biru = $('#biru').val();
    	if (panjang != undefined && lebar != undefined && tinggi != undefined && substrate != undefined) {
    		if (panjang != "" && lebar != "" && tinggi != "" && substrate != "") {
    			if (putih != "" || merah != "" || biru != "") {
@@ -42,18 +42,18 @@ function hitungLumen () {
 					lumen = (putih*200)+(merah*90)+(biru*40);
 				}
 				var lumenLiter = lumen / volAir;
-				document.getElementById("hasilLumen").innerHTML = lumenLiter.toFixed(2);
+				$("#hasilLumen").text(lumenLiter.toFixed(2));
 				if (lumenLiter>=15 && lumenLiter<=25) {
-					document.getElementById("kategori").innerHTML = "Low Light";
+					$("#kategori").text("Low Light");
 				}
 				else if (lumenLiter>25 && lumenLiter<=50) {
-					document.getElementById("kategori").innerHTML = "Medium Light";
+					$("#kategori").text("Medium Light");
 				}
 				else if (lumenLiter>50) {
-					document.getElementById("kategori").innerHTML = "High Light";
+					$("#kategori").text("High Light");
 				}
 				else {
-					document.getElementById("kategori").innerHTML = "Not Enough Light"
+					$("#kategori").text("Not Enough Light");
 				}
 			}
 		} else {
@@ -66,10 +66,10 @@ function hitungLumen () {
 
 function dayaLed() {
 	var daya;
-	var light = document.getElementById("light").value;
+	var light = $("#light").val();
 	if (volAir != undefined) {
 		daya = volAir*light/110;
-		document.getElementById("rekomendasi").innerHTML = Math.ceil(daya) +" Watt";
+		$("#rekomendasi").text(Math.ceil(daya) +" Watt");
 	} else {
 		alert("Masukkan spesifikasi tank");
 	}
